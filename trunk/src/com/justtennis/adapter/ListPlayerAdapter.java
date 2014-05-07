fbc15d942308ce9a9cfd369bd7e35feb9674054f
roca.david@gmail.com <roca.david@gmail.com@dac058d7-4120-8cb2-69ef-2c1dc36baf9d>
2014-05-07 21:51:10 +0000

    List Player - Affichage de la location de chaque player

--- trunk/src/com/justtennis/adapter/ListPlayerAdapter.java
+++ trunk/src/com/justtennis/adapter/ListPlayerAdapter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +17,18 @@ import com.justtennis.ApplicationConfig;
 import com.justtennis.R;
 import com.justtennis.activity.ListPlayerActivity;
 import com.justtennis.business.ListPlayerBusiness;
+import com.justtennis.db.service.ClubService;
 import com.justtennis.db.service.RankingService;
+import com.justtennis.db.service.TournamentService;
+import com.justtennis.domain.Club;
 import com.justtennis.domain.Player;
+import com.justtennis.domain.Tournament;
+import com.justtennis.domain.Player.PLAYER_TYPE;
 import com.justtennis.domain.Ranking;
 import com.justtennis.filter.ListPlayerByTypeFilter;
 import com.justtennis.manager.ContactManager;
 import com.justtennis.notifier.NotifierMessageLogger;
+import com.justtennis.parser.LocationParser;
 
 public class ListPlayerAdapter extends ArrayAdapter<Player> {
 
@@ -29,7 +36,10 @@ public class ListPlayerAdapter extends ArrayAdapter<Player> {
 	private ListPlayerActivity activity;
 	private Filter filter = null;
 	private ArrayList<Player> valueOld;
-	private HashMap<Long, Ranking> mapRanking;
+	private HashMap<Long, Ranking> rankingService;
+	private ClubService clubService;
+	private TournamentService tournamentService;
+	private LocationParser locationParser;
 
 	public ListPlayerAdapter(ListPlayerActivity activity, List<Player> value) {
 		super(activity, R.layout.list_player_row, android.R.id.text1, value);
@@ -46,7 +56,11 @@ public class ListPlayerAdapter extends ArrayAdapter<Player> {
 				notifyDataSetChanged();
 			}
 		}, valueOld);
-		mapRanking = new RankingService(activity, NotifierMessageLogger.getInstance()).getMapById();
+		NotifierMessageLogger notifier = NotifierMessageLogger.getInstance();
+		rankingService = new RankingService(activity, notifier).getMapById();
+		clubService = new ClubService(activity, notifier);
+		tournamentService = new TournamentService(activity, notifier);
+		locationParser = LocationParser.getInstance(activity, notifier);
 	}
 
 	@Override
@@ -61,8 +75,8 @@ public class ListPlayerAdapter extends ArrayAdapter<Player> {
 
 	public static class ViewHolder {
 		public Player player;
-		public TextView firstname;
-		public TextView lastname;
+		public TextView name;
+		public TextView clubName;
 		public ImageView imageSend;
 		public ImageView imagePlayer;
 		public ImageView imageDelete;
@@ -84,25 +98,25 @@ public class ListPlayerAdapter extends ArrayAdapter<Player> {
 
 		int iVisibility = (business.isUnknownPlayer(v) ? View.GONE : View.VISIBLE);
 
-		ImageView imageSend = (ImageView) rowView.findViewById(R.id.iv_send);
 		ImageView imagePlayer = (ImageView) rowView.findViewById(R.id.iv_player);
+//		ImageView imageSend = (ImageView) rowView.findViewById(R.id.iv_send);
 		ImageView imageDelete = (ImageView) rowView.findViewById(R.id.iv_delete);
 		TextView ranking = (TextView) rowView.findViewById(R.id.tv_ranking);
-		TextView firstname = (TextView) rowView.findViewById(R.id.tv_fistname);
-		TextView lastname = (TextView) rowView.findViewById(R.id.tv_lastname);
+		TextView name = (TextView) rowView.findViewById(R.id.tv_name);
+		TextView clubName = (TextView) rowView.findViewById(R.id.tv_club_name);
 
 		imageDelete.setVisibility(iVisibility);
-		lastname.setVisibility(iVisibility);
 
-		Ranking r = mapRanking.get(v.getIdRanking()); 
-		imageSend.setTag(v);
+		Ranking r = rankingService.get(v.getIdRanking()); 
 		imagePlayer.setTag(v);
+//		imageSend.setTag(v);
 		imageDelete.setTag(v);
 		ranking.setText(r == null ? "" : r.getRanking());
-		firstname.setText(v.getFirstName());
-		lastname.setText(v.getLastName());
-		
-//		if (v.getPhoto()!=null) {
+		name.setText(Html.fromHtml("<b>" + v.getFirstName() + "</b> " + v.getLastName()));
+
+		initializeLocation(v, clubName);
+
+	//		if (v.getPhoto()!=null) {
 //			imagePlayer.setImageBitmap(v.getPhoto());
 //		}
 		if (v.getIdGoogle()!=null && v.getIdGoogle().longValue()>0l) {
@@ -113,23 +127,23 @@ public class ListPlayerAdapter extends ArrayAdapter<Player> {
 		}
 
 		if (ApplicationConfig.SHOW_ID) {
-			firstname.setText(firstname.getText() + " [" + v.getId() + "|" + v.getIdExternal() + "]");
+			name.setText(name.getText() + " [" + v.getId() + "|" + v.getIdExternal() + "]");
 		}
 
-		switch (activity.getMode()) {
-			case EDIT:
-				if (v.getIdExternal()!=null && v.getIdExternal()>0) {
-					imageSend.setVisibility(View.GONE);
-				}
-				else {
-					imageSend.setVisibility(iVisibility);
-				}
-				break;
-			case INVITE:
-				imageSend.setVisibility(View.GONE);
-				imageDelete.setVisibility(View.GONE);
-				break;
-		}
+//		switch (activity.getMode()) {
+//			case EDIT:
+//				if (v.getIdExternal()!=null && v.getIdExternal()>0) {
+//					imageSend.setVisibility(View.GONE);
+//				}
+//				else {
+//					imageSend.setVisibility(iVisibility);
+//				}
+//				break;
+//			case INVITE:
+//				imageSend.setVisibility(View.GONE);
+//				imageDelete.setVisibility(View.GONE);
+//				break;
+//		}
 	    return rowView;
 	}
 
@@ -142,5 +156,15 @@ public class ListPlayerAdapter extends ArrayAdapter<Player> {
 
 		valueOld.clear();
 		valueOld.addAll(this.value);
+	}
+
+	private void initializeLocation(final Player v, TextView clubName) {
+		String[] address = locationParser.toAddress(v);
+		if (address != null) {
+			clubName.setText(address[0]);
+			clubName.setVisibility(View.VISIBLE);
+		} else {
+			clubName.setVisibility(View.GONE);
+		}
 	}
 }
